2176f780657d59ad79bc072c65facdf2bf1d5744
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2023-10-12 10:04:12 +0600

    Home page

--- src/app/(homelayout)/page.tsx
+++ src/app/(homelayout)/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex">
+    <div className="flex flex-col">
       <div className="relative">
         <Image
           className="w-full max-h-[800px]"
@@ -11,14 +11,14 @@ export default function Home() {
           width={10000}
           alt="Hero image"
         />
-        <div className="absolute top-1/2 left-[700px] transform -translate-y-1/2">
+        <div className="absolute top-1/2 md:top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center md:left-[800px] md:text-left md:transform-none">
           <div>
             <h1 className="mb-3 text-4xl font-bold text-gray-900 md:text-5xl md:leading-tight md:font-extrabold">
               Quality Wallpapers
             </h1>
-            <h1 className="text-2xl ms-7">Paintings For Home</h1>
+            <h1 className="text-2xl md:ms-7">Paintings For Home</h1>
           </div>
-          <div className="mt-10 flex flex-col justify-start mb-3 space-x-0 space-y-2 text-xs text-gray-600 md:flex-row md:justify-center md:space-x-8 md:space-y-0">
+          <div className="mt-10 space-y-3 text-xs text-gray-600 md:mt-5 md:flex md:flex-row md:justify-center md:space-x-8 md:space-y-0">
             <div className="flex items-center">
               <svg
                 viewBox="0 0 20 20"
@@ -26,9 +26,9 @@ export default function Home() {
                 className="w-4 h-4 mr-1 text-green-600"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 ></path>
               </svg>
               No credit card required
@@ -40,9 +40,9 @@ export default function Home() {
                 className="w-4 h-4 mr-1 text-green-600"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 ></path>
               </svg>
               No commitment
@@ -54,9 +54,9 @@ export default function Home() {
                 className="w-4 h-4 mr-1 text-green-600"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 ></path>
               </svg>
               Cancel anytime
